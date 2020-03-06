af0f0be1d8ab92ce12de4a3c07b55e9f9f2fb661
Jethro Cheng <jethrocheng@gmail.com>
2020-03-06 17:04:55 -0500

    Updates

---
+++ precache-manifest.50bc93fa0e887e21243280cabbe26102.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0678f7f254ef098131a7c176310d4df",
+    "url": "/my-face-api/index.html"
+  },
+  {
+    "revision": "584c2fb00d2332384adc",
+    "url": "/my-face-api/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ef18b916e7ed765faaac",
+    "url": "/my-face-api/static/css/main.7123a1d1.chunk.css"
+  },
+  {
+    "revision": "584c2fb00d2332384adc",
+    "url": "/my-face-api/static/js/2.b1fda2f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-face-api/static/js/2.b1fda2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef18b916e7ed765faaac",
+    "url": "/my-face-api/static/js/main.cb98d5cd.chunk.js"
+  },
+  {
+    "revision": "c4ad75fba6392368c556",
+    "url": "/my-face-api/static/js/runtime-main.fef532a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-face-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
